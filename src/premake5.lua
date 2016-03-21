7f9b9e494afcd336f26f6b3d373fff2f69774bd5
Xavier Bergeron <xavbergeron@gmail.com>
2016-03-20 22:29:40 -0400

    Make premake file slightly more friendly

--- src/premake5.lua
+++ src/premake5.lua
@@ -1,37 +1,46 @@
 solution "Fuck_Awesomium"
-   language "C++"
-   location "project"
-   targetdir "build/release"
+    language "C++"
+    location "project"
+    targetdir "build/release"
 
-   configuration "vs*" -- speed shit
-   --gr- = no typeinfo
-   buildoptions({"/Qpar", "/Qfast_transcendentals", "/GL", "/Ot", "/Gm", "/MP", "/Gy", "/Gw"})
-   linkoptions { "/OPT:REF", "/OPT:ICF", "/LTCG"}
+    configuration "vs*" -- speed shit
+    --gr- = no typeinfo
+    buildoptions({"/Qpar", "/Qfast_transcendentals", "/GL", "/Ot", "/Gm", "/MP", "/Gy", "/Gw"})
+    linkoptions { "/OPT:REF", "/OPT:ICF", "/LTCG"}
 
-   flags { "Optimize", "NoMinimalRebuild", "NoFramePointer", "EnableSSE2", "FloatFast", "NoBufferSecurityCheck"}
+    flags { "Optimize", "NoMinimalRebuild", "NoFramePointer", "EnableSSE2", "FloatFast", "NoBufferSecurityCheck"}
 
-   vpaths {
-      ["Header Files/*"] = "src/**.h",
-      ["Source Files/*"] = "src/**.cpp",
-   }
+    vpaths {
+        ["Header Files/*"] = "src/**.h",
+        ["Source Files/*"] = "src/**.cpp",
+    }
 
-   kind "SharedLib"
+    kind "SharedLib"
 
-   configurations { "Debug", "Release" }
+    configurations { "Debug", "Release" }
 
-   files { "src/**.h", "src/**.cpp" }
-   
-   includedirs { "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//game//shared", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//public", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//public//tier3", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//public//tier2", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//public//tier1", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//public//tier0", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//game//client", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//tier1", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//tier0", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//game", "C://Users//Leystryku//Desktop//Folders//Source-Engine//src//"}
-   libdirs {"C://Users//Leystryku//Desktop//Folders//Source-Engine//src//lib//public","C://Users//Leystryku//Desktop//Folders//Source-Engine//src//lib//"}
-   
-   -- A project defines one build target
-   project "Fuck_Awesomium"
-      targetname "fuck_awesomium"
+    files { "src/**.h", "src/**.cpp" }
 
-      configuration "Release"
-         defines { "NDEBUG", "_GENERIC" }
-         
-      configuration "Debug"
-         defines { "DEBUG", "_GENERIC" }
-         flags { "Symbols", "EnableSSE2" }
-		 targetdir "build/debug"
+    local sdk_dir = "C:/sdk13/mp/src/" -- Change this to suit your setup
+
+    local function sdk(s) return sdk_dir..s end
+
+    includedirs { sdk"public",       sdk"public/tier0", sdk"public/tier1",
+                  sdk"public/tier2", sdk"public/tier3", sdk"public/tier0",
+                  sdk"game",         sdk"game/client",  sdk"game/shared",
+                  sdk"tier1",        sdk"tier0",        sdk"common",
+				  sdk "", }
+
+    libdirs     { sdk"lib", sdk"lib/public" }
+
+    -- A project defines one build target
+    project "Fuck_Awesomium"
+        configuration "Release"
+            defines { "NDEBUG", "_GENERIC" }
+            targetname "awesomium"
+            targetdir "../dieawesomium/bin"
+
+        configuration "Debug"
+            defines { "DEBUG", "_GENERIC" }
+            flags { "Symbols", "EnableSSE2" }
+            targetdir "build/debug"
